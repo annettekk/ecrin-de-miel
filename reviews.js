babde10be8c69ada3e29b05c77a0a2729d9e2969
Anna K <akuroshenkova@gmail.com>
2023-07-06 14:26:32 +0100

    added review date

--- reviews.js
+++ reviews.js
@@ -1,10 +1,12 @@
 const reviewForm = document.getElementById("addReviewForm");
 const containerElement = document.getElementById("reviews");
 let newReviewText = document.getElementById("review");
+document.getElementById('date').value = Date();
 let reviews = [];
 
-function Review(reviewtext) {
+function Review(reviewtext, date) {
     this.reviewtext = reviewtext;
+    this.date = date;
     //this.render();
     this.pushReviews = function() {
       reviews.push(this)
@@ -15,14 +17,17 @@ function Review(reviewtext) {
 Review.prototype.render = function () {
     
     const article = document.createElement('article');
-    const space = document.createElement('space')
-    space.textContent = '...........................................................'
+    const date = document.createElement('date')
+    article.setAttribute ('id', 'newrev');
+    date.setAttribute ('id', 'newdate');
+    
+    date.textContent = this.date;
     article.textContent = this.reviewtext;
     console.log(article.textContent)
     console.log(containerElement)
     containerElement.append(article);
-    containerElement.append(space)
-    
+    containerElement.append(date)
+  
     
         //cookieStands.push(this)
 }
@@ -31,10 +36,12 @@ reviewForm.addEventListener("submit", function (e) {
     e.preventDefault();
     
     const theReview = e.target.newreview.value
+    const theDate = e.target.date.value
     console.log(theReview)
 
-     const newReview = new Review(theReview);
+     const newReview = new Review(theReview, theDate);
      newReview.render();
 
     reviewForm.reset();
+    document.getElementById('date').value = Date();
     })
